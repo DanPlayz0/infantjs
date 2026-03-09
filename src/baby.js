d7db1d3ef17e92195b85bf97d647418a8ebe1a80
DanPlayz0 <damuffin36@gmail.com>
2026-03-09 13:57:37 -0700

    wip

--- src/baby.js
+++ src/baby.js
@@ -1,3 +1,4 @@
+import interpret from "./interpreter.js";
 import parse from "./parser.js";
 import * as fs from "node:fs";
 
@@ -6,12 +7,12 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-
-const name = process.argv[2];
-const sourceCode = fs.readFileSync(name, "utf-8");
-const parseTree = parse(sourceCode);
-if (parseTree.succeeded()) {
-  console.log("Parse succeeded!");
-} else {
-  console.error(parseTree);
+try {
+  const name = process.argv[2];
+  const sourceCode = fs.readFileSync(name, "utf-8");
+  const match = parse(sourceCode); // if it failed, it would have thrown
+  interpret(match);
+} catch (e) {
+  console.error(e.message);
+  process.exit(1);
 }
