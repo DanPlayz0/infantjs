828d5502300ed08b0022f80a2e7d01ee87eafe07
stacks-Gabe <ggarci78@lion.lmu.edu>
2026-04-23 19:07:21 -0700

    updated core.js
    pls check if this works

--- src/core.js
+++ src/core.js
@@ -3,151 +3,77 @@ export function program(body) {
 }
 
 export function printStmt(argument) {
-  return {
-    kind: "PrintStatement",
-    arguments: [argument],
-  }
+  return { kind: "PrintStatement", arguments: [argument] }
 }
 
 export function variable(name, type) {
-  return {
-    kind: "Variable",
-    name,
-    type,
-  }
+  return { kind: "Variable", name, type }
 }
 
 export function functionDecl(fun, body) {
-  return {
-    kind: "FunctionDeclaration",
-    function: fun,
-    body,
-  }
+  return { kind: "FunctionDeclaration", function: fun, body }
 }
 
 export function functionObject(name, params) {
-  return {
-    kind: "FunctionObject",
-    name,
-    params,
-  }
+  return { kind: "FunctionObject", name, params }
 }
 
 export function functionCall(callee, args, type) {
-  return {
-    kind: "FunctionCall",
-    callee,
-    arguments: args,
-    type,
-  }
+  return { kind: "FunctionCall", callee, arguments: args, type }
 }
+
 export function letStmt(variable, initializer) {
-  return {
-    kind: "LetStatement",
-    variable,
-    initializer,
-  }
+  return { kind: "LetStatement", variable, initializer }
 }
 
 export function assignStmt(target, source) {
-  return {
-    kind: "AssignStatement",
-    target,
-    source,
-  }
+  return { kind: "AssignStatement", target, source }
 }
 
 export function ifStmt(test, consequent, alternate) {
-  return {
-    kind: "IfStatement",
-    test,
-    consequent,
-    alternate,
-  }
+  return { kind: "IfStatement", test, consequent, alternate }
 }
 
 export function whileStmt(test, body) {
-  return {
-    kind: "WhileStatement",
-    test,
-    body,
-  }
+  return { kind: "WhileStatement", test, body }
 }
 
 export function binaryExp(left, operator, right, type) {
-  return {
-    kind: "BinaryExpression",
-    operator,
-    left,
-    right,
-    type,
-  }
+  return { kind: "BinaryExpression", operator, left, right, type }
 }
 
 export function unaryExp(operator, argument, type) {
-  return {
-    kind: "UnaryExpression",
-    operator,
-    argument,
-    type,
-  }
+  return { kind: "UnaryExpression", operator, argument, type }
 }
 
 export function randomStmt(minimum, maximum) {
-  return {
-    kind: "RandomStatement",
-    type: "number",
-    minimum,
-    maximum,
-  }
+  return { kind: "RandomStatement", type: "number", minimum, maximum }
 }
 
 export function returnStmt(value) {
-  return {
-    kind: "ReturnStatement",
-    value,
-  }
+  return { kind: "ReturnStatement", value }
 }
 
 export function inputStmt(prompt = undefined) {
-  return {
-    kind: "InputStatement",
-    prompt,
-    type: "string",
-  }
+  return { kind: "InputStatement", prompt, type: "string" }
 }
 
 export function sleepStmt(duration) {
-  return {
-    kind: "SleepStatement",
-    duration,
-  }
+  return { kind: "SleepStatement", duration }
 }
+
 export function castStmt(value, targetType) {
-  return {
-    kind: "CastStatement",
-    value,
-    type: targetType,
-  }
+  return { kind: "CastStatement", value, type: targetType }
 }
+
 export function floorStmt(value) {
-  return {
-    kind: "FloorStatement",
-    value,
-    type: "number",
-  }
+  return { kind: "FloorStatement", value, type: "number" }
 }
+
 export function ceilStmt(value) {
-  return {
-    kind: "CeilStatement",
-    value,
-    type: "number",
-  }
+  return { kind: "CeilStatement", value, type: "number" }
 }
+
 export function roundStmt(value) {
-  return {
-    kind: "RoundStatement",
-    value,
-    type: "number",
-  }
+  return { kind: "RoundStatement", value, type: "number" }
 }
